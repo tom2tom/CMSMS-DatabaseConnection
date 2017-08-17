1b22f7992cb169d891694d703171e0fa8ca0654c
tom2tom <tpgww@onepost.net>
2017-08-17 17:55:08 +1000

    add licence add property setter, getter for error reporting

--- class.EmptyResultSet.php
+++ class.EmptyResultSet.php
@@ -1,20 +1,88 @@
 <?php
+/*
+-------------------------------------------------------------------------
+Module: \CMSMS\Database\mysqli\EmptyResultSet (C) 2017 Robert Campbell
+         <calguy1000@cmsmadesimple.org>
+A class to represent an empty recordset
+-------------------------------------------------------------------------
+CMS Made Simple (C) 2004-2017 Ted Kulp <wishy@cmsmadesimple.org>
+Visit our homepage at: http:www.cmsmadesimple.org
+-------------------------------------------------------------------------
+BEGIN_LICENSE
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
 
-/**
- * A class to describe an empty recordset.
- *
- * @ignore
- */
+However, as a special exception to the GPL, this software is distributed
+as an addon module to CMS Made Simple.  You may not use this software
+in any Non GPL version of CMS Made simple, or in any version of CMS
+Made simple that does not indicate clearly and obviously in its admin
+section that the site was built with CMS Made simple.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+Or read it online: http:www.gnu.org/licenses/licenses.html#GPL
+END_LICENSE
+-------------------------------------------------------------------------
+*/
 
 namespace CMSMS\Database;
 
 /**
- * A class to describe a special (empty) recordset.
+ * A class to represent a special (empty) recordset (maybe after an error).
  *
  * @ignore
  */
 final class EmptyResultSet extends ResultSet
 {
+    /**
+     * @ignore
+     */
+    private $errno = 0;
+    private $error = '';
+
+    /**
+     * @ignore
+     */
+    public function __set($key, $val)
+    {
+        switch ($key) {
+         case 'errno':
+            $this->errno = $val;
+            break;
+         case 'error':
+         case 'errmsg':
+            $this->error = $val;
+            break;
+        }
+    }
+
+    /**
+     * @ignore
+     */
+    public function __get($key)
+    {
+        switch ($key) {
+         case 'errno':
+            return $this->errno;
+         case 'error':
+         case 'errmsg':
+            return $this->error;
+         case 'EOF':
+            return true;
+         case 'count':
+            return 0;
+         default:
+            return null;
+        }
+    }
+
     /**
      * @ignore
      */
